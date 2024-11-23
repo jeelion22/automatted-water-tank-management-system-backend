96a3c27440c7541b18ceff89cb098993eaf20961
jeeva <jeelion22@gmail.com>
2024-11-23 14:01:26 +0530

    use mongodb

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const app = require("./app");
+const config = require("./utils/config");
+const mongoose = require("mongoose");
 
-app.listen(3200, () => {
-  console.log("Server is running on port 3200");
-});
+console.log("Connecting to MongoDB...");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const PORT = config.SERVER_PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log("Error connecting to MongoDB", error.message));
